d9876b27d20710c8241284faff4b00e277f7e08c
vixplows <vicky@vickyplows.co.uk>
2017-05-28 10:16:02 +0100

    Amend function for test 3 and 4 to pass tests

---
+++ pet_shop.rb
@@ -0,0 +1,13 @@
+def pet_shop_name(shop)
+  return shop[:name]
+end
+
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop, cash)
+  return shop[:admin][:total_cash]+=(cash)
+end
+
+
